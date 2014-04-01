e5f98488727e63ecd4229d785ce1de27564f8a20
sbolding <simonrbolding@gmail.com>
2014-04-01 12:09:14 -0500

    derpo

--- holo/Controller.h
+++ holo/Controller.h
@@ -19,7 +19,7 @@ namespace HoController
 	const bool ADAPTIVE_REFINEMENT = true;
 	const bool FORCE_REFINEMENT = false;
 	const double FRACTION_CELLS_TO_REFINE = 0.3;
-	const bool REFINE_ACROSS_MU_ZERO = true;
+	const bool REFINE_ACROSS_MU_ZERO = false;
 	const bool USE_MAX_JUMP_ERROR = true; //use the max jump error of all sides as error indicator for refinement
 	const bool MAX_REFINEMENT_LEVEL_SET = false;
 	const unsigned int MAX_REFINEMENT_LEVEL = 6;
